7a806219d2f697dea53a7f32411a86911ee78196
CongXiaoNan <37144674+CongXiaoNan@users.noreply.github.com>
2019-10-31 08:52:22 +0800

    Delete home.js

--- api/common/home.js
+++
@@ -1,9 +0,0 @@
-import request from '../index'
-
-export const config = {
-
-}
-
-export default function req (funcName, data, isToken = true, isCancel = true) {
-  return request(config, funcName, data, isToken, isCancel)
-}
